3922b99c376f1be4f7c044ee1d9f1a5831d45a2c
John <jackbball12@gmail.com>
2023-10-14 19:30:30 -0400

    Mobile Development

--- src/skills-data.js
+++ src/skills-data.js
@@ -3,9 +3,9 @@ import C from "./assets/skills/C.svg"
 import Python from "./assets/skills/Python.svg"
 import Java from "./assets/skills/Java.svg"
 import CSS from "./assets/skills/CSS.svg"
-// import Javascript from "./assets/skills/Javascript.svg"
+import Javascript from "./assets/skills/Javascript.svg"
 import React from "./assets/skills/React.svg"
-import Git from "./assets/skills/Git.svg"
+// import Git from "./assets/skills/Git.svg"
 
 const languages = [
     {
@@ -28,18 +28,18 @@ const languages = [
         skill: "HTML/CSS",
         src: CSS
     },
-    // {
-    //     skill: "JavaScript",
-    //     src: Javascript
-    // },
+    {
+        skill: "JavaScript",
+        src: Javascript
+    },
     {
         skill: "React",
         src: React
     },
-    {
-        skill: "Git",
-        src: Git
-    }
+    // {
+    //     skill: "Git",
+    //     src: Git
+    // }
 ]
 
 export default languages;
